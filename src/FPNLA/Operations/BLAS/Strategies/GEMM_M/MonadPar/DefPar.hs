ee9872aed0ba04e0d53e54434a91a8eb4589b977
Mauro Blanco <blancomau@gmail.com>
2016-08-20 20:49:52 -0300

    DefSeq y DefPar para vector mutable

--- src/FPNLA/Operations/BLAS/Strategies/GEMM_M/MonadPar/DefPar.hs
+++ src/FPNLA/Operations/BLAS/Strategies/GEMM_M/MonadPar/DefPar.hs
@@ -16,6 +16,8 @@ import           FPNLA.Operations.Parameters                (Elt, blasResultM,
 import Control.Concurrent.MVar
 import Control.Concurrent
 import Control.Exception
+import Control.Monad (foldM)
+
 --import Debug.Trace (trace)
 
 instance (Elt e, Matrix IO m e, Vector IO v e, RowMatrixVector IO m v e) => GEMM_M IO DefPar_MP m v e where
@@ -24,30 +26,26 @@ instance (Elt e, Matrix IO m e, Vector IO v e, RowMatrixVector IO m v e) => GEMM
             (_, _) <- dim_m mA
             (_, p) <- dim_m mB
             let matMultIJ i j =
-                    foldl (\mr x ->
+                    foldM (\r x ->
                         do
-                            r <- mr
                             maIX <- elem_m i x mA
                             mbXJ <- elem_m j x mB
-                            return $ r + maIX * mbXJ) (return 0) [0 .. p - 1]
---            rows :: [v e] <- toRows_vm mC
---            mvars <- traverse (\(r, i) -> do
---                done <- newEmptyMVar
---                _ <- forkIO (do
---                    v' <- update_v (\j ->
---                        do
---                            multIJ <- matMultIJ i j
---                            mcIJ <- elem_m i j mC
---                            return $ alpha * multIJ + beta * mcIJ) r
---                    _ <- evaluate v'
---                    putMVar done ()
---                    return ())
---                return done) $ zip rows [0 .. ]
---            _ <- traverse takeMVar mvars
---            return $ blasResultM mC
-            blasResultM <$> update_m (\i j ->
-                do
-                    multIJ <- matMultIJ i j
-                    mcIJ <- elem_m i j mC
-                    return $ alpha * multIJ + beta * mcIJ) mC
+                            let res = r + maIX * mbXJ
+                            _ <- evaluate res
+                            return res) 0 [0 .. p - 1]
+            rows :: [v e] <- toRows_vm mC
+            mvars <- traverse (\(r, i) -> do
+                done <- newEmptyMVar
+                _ <- forkIO (do
+                    v' <- update_v (\j ->
+                        do
+                            multIJ <- matMultIJ i j
+                            mcIJ <- elem_m i j mC
+                            return $ alpha * multIJ + beta * mcIJ) r
+                    _ <- evaluate v'
+                    putMVar done ()
+                    return ())
+                return done) $ zip rows [0 .. ]
+            _ <- traverse takeMVar mvars
+            return $ blasResultM mC
     gemm_m _ _ _ _ _ _ = error "TODO!"
